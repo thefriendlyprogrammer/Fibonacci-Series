953ea6d7fc2f9f157aead9646c0684c5bec1e705
Alok Kushwaha <95715756+thefriendlyprogrammer@users.noreply.github.com>
2022-04-22 14:30:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("\"Fibonacci Series\"");
+                int n1 = 0, n2 = 1, i, number;
+                Console.WriteLine("Enter the number (where number > 2): ");
+                number = int.Parse(Console.ReadLine());
+                Console.Write(n1 + ", " + n2 + ", ");
+                for (i = 2; i < number; ++i)
+                {
+                    int n3;
+                    n3 = n1 + n2;
+                    Console.Write(n3 + ", ");
+                    n1 = n2;
+                    n2 = n3;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
